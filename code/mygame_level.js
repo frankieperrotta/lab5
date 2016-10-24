d10e44d7fbed062c9e78ced1f4e3c2c7714db597
frankieperrotta <frankieperrotta@Frankies-MacBook-Pro.local>
2016-10-23 22:09:32 -0400

    add collision, player speed, player restart when hits lava. and coin wabble(probably delete in next commit)

--- code/mygame_level.js
+++ code/mygame_level.js
@@ -14,12 +14,16 @@
    "  x                                                                   x      x  ",
    "  x                                                 y                 x      x  ",
    "  x                                                                   x      x  ",
-   "  x          xxxx                       xxxxxx      x                 x      x  ",
-   "  x  @       x  x      y                x    x      x                 x      x  ",
-   "  xxxxxxxxxxxx  xxxxxxxxxxxxxxx   xxxxxxx    xx    xxx     xxxxxxx    x      x  ",
-   "                              x   x           x    x x     x     x!!!!          ",
-   "                              x!!!x           x!!!!x x!!!!!x                    ",
+   "  x                                     xxxxxx      x                 x      x  ",
+   "  x  @                 y                x    x      x                 x      x  ",
+   "  xxxxxxxxxxx    xxxxxxxxxxxxxx   xxxxxxx    xx    xxx     xxxxxxx    x      x  ",
+   "            x!!!!x            x   x           x    x x     x     x!!!!x         ",
+   "            xxxxxx            x!!!x           x!!!!x x!!!!!x     xxxxxx         ",
    "                              x!!!x           x!!!!x x!!!!!x                    ",
    "                              xxxxx           xxxxxx xxxxxxx                    ",
    "                                                                                ",
    "                                                                                "],
+   ];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
